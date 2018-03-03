6a755ffe3987502606e0fb74add52fc8544bc938
Praaveen <praaveen.vr@spritle.com>
2018-03-03 21:02:41 +0530

    API for create, show, list

---
+++ server/models/todoitem.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var TodoItem = sequelize.define('TodoItem', {
+    content: {type: DataTypes.STRING,
+              allowNull: false},
+    complete: {type: DataTypes.BOOLEAN,
+        defaultValue: false
+  }
+  });
+  TodoItem.associate = function(models) {
+      TodoItem.belongsTo(models.Todo,{
+          foreignKey: 'todoId',
+          onDelete: 'CASCADE'});
+
+  };
+  return TodoItem;
+};
