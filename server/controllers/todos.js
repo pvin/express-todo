514dc94e78dfab236dee702e0b9e8f34f0a0aaeb
praaveen <praaveen.ece@gmail.com>
2018-03-04 22:40:35 +0530

    update and delete

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -38,5 +38,49 @@ module.exports = {
 
     }).catch(error => res.status(400).send(error));
 
+},
+
+    update(req, res){
+    return Todo.findById(req.params.todoId,{
+        attributes : ['id', 'title'],
+        include: [{
+            model: TodoItem,
+            as: 'todoItems'
+        }]
+    })
+            .then(todo => {
+            if (!todo) {
+        return res.status(404).send({
+            message: 'Todo Not Found',
+        });
+    }
+    return todo
+            .update({
+                title: req.body.title
+            })
+            .then(() => res.status(200).send(todo))  // Send back the updated todo.
+            .catch((error) => res.status(400).send(error));
+     })
+            .catch((error) => res.status(400).send(error));
+    },
+
+    destroyy(req, res){
+      return Todo
+            .findById(req.params.todoId,{attributes : ['id', 'title']})
+            .then(todo => {
+            if (!todo) {
+        return res.status(400).send({
+            message: 'Todo Not Found',
+        });
+    }
+    return todo
+            .destroy({ force: true })
+            .then(() => res.status(204).send())
+            .catch(error => res.status(400).send(error));
+        })
+       .catch(error => res.status(401).send(error));
 }
+
+
+
 };
